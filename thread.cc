bc212b0b533422a59c2fca025445270d24b32653
Caoimhe Chaos <caoimhechaos@protonmail.com>
2012-12-16 19:19:16 +0100

    Add a basic implementation of various types of thread (regular thread template, closure runner, thread queue with worker threads, etc).

---
+++ thread.cc
@@ -0,0 +1,93 @@
+/*-
+ * Copyright (c) 2012 Caoimhe Chaos <caoimhechaos@protonmail.com>,
+ *                    Ancient Solutions. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions  of source code must retain  the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions  in   binary  form  must   reproduce  the  above
+ *    copyright  notice, this  list  of conditions  and the  following
+ *    disclaimer in the  documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * THIS  SOFTWARE IS  PROVIDED BY  ANCIENT SOLUTIONS  AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO,  THE IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS
+ * FOR A  PARTICULAR PURPOSE  ARE DISCLAIMED.  IN  NO EVENT  SHALL THE
+ * FOUNDATION  OR CONTRIBUTORS  BE  LIABLE FOR  ANY DIRECT,  INDIRECT,
+ * INCIDENTAL,   SPECIAL,    EXEMPLARY,   OR   CONSEQUENTIAL   DAMAGES
+ * (INCLUDING, BUT NOT LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE,  DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT  LIABILITY,  OR  TORT  (INCLUDING NEGLIGENCE  OR  OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "threadpool.h"
+
+#include <QtCore/QString>
+#include <pthread.h>
+
+namespace threadpp
+{
+ThreadError::ThreadError(int err)
+: err_(err)
+{
+}
+
+QString
+ThreadError::String()
+{
+	char buf[128];
+	strerror_r(err_, buf, 128);
+	return QString(buf);
+}
+
+void*
+Thread::CallHelper(void *thread)
+{
+	Thread* t = reinterpret_cast<Thread*>(thread);
+	t->Run();
+	return NULL;
+}
+
+Thread::Thread()
+: cancelled_(false)
+{
+	int err;
+	pthread_attr_t attr;
+	err = pthread_attr_init(&attr);
+	if (err)
+		throw new ThreadError(err);
+	err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	if (err)
+		throw new ThreadError(err);
+	err = pthread_create(&thread_, &attr, &Thread::CallHelper, this);
+	if (err)
+		throw new ThreadError(err);
+	err = pthread_attr_destroy(&attr);
+	if (err)
+		throw new ThreadError(err);
+}
+
+Thread::~Thread()
+{
+	pthread_join(thread_, NULL);
+}
+
+void
+Thread::Cancel()
+{
+	cancelled_ = true;
+}
+
+bool
+Thread::IsCancelled()
+{
+	return cancelled_;
+}
+}  // namespace threadpp
---
+++ threadqueue.cc
@@ -0,0 +1,88 @@
+/*-
+ * Copyright (c) 2012 Caoimhe Chaos <caoimhechaos@protonmail.com>,
+ *                    Ancient Solutions. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions  of source code must retain  the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions  in   binary  form  must   reproduce  the  above
+ *    copyright  notice, this  list  of conditions  and the  following
+ *    disclaimer in the  documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * THIS  SOFTWARE IS  PROVIDED BY  ANCIENT SOLUTIONS  AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO,  THE IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS
+ * FOR A  PARTICULAR PURPOSE  ARE DISCLAIMED.  IN  NO EVENT  SHALL THE
+ * FOUNDATION  OR CONTRIBUTORS  BE  LIABLE FOR  ANY DIRECT,  INDIRECT,
+ * INCIDENTAL,   SPECIAL,    EXEMPLARY,   OR   CONSEQUENTIAL   DAMAGES
+ * (INCLUDING, BUT NOT LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE,  DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT  LIABILITY,  OR  TORT  (INCLUDING NEGLIGENCE  OR  OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <google/protobuf/stubs/common.h>
+#include "threadpool.h"
+
+#include <QtCore/QQueue>
+#include <QtCore/QWaitCondition>
+#include <QtCore/QMutex>
+#include <QtCore/QMutexLocker>
+
+namespace threadpp
+{
+ThreadQueue::ThreadQueue()
+{
+}
+
+ThreadQueue::~ThreadQueue()
+{
+}
+
+void
+ThreadQueue::Add(google::protobuf::Closure* t)
+{
+	QMutexLocker l(&queue_lock_);
+	queued_threads_.enqueue(t);
+	task_availability_.wakeAll();
+}
+
+google::protobuf::Closure*
+ThreadQueue::GetNextTask()
+{
+	queue_lock_.lock();
+
+	while (queued_threads_.isEmpty())
+		task_availability_.wait(&queue_lock_);
+
+	return queued_threads_.dequeue();
+}
+
+void
+ThreadQueue::WaitUntilEmpty()
+{
+	queue_lock_.lock();
+
+	while (!queued_threads_.isEmpty())
+	{
+		queue_lock_.unlock();
+		queue_lock_.lock();
+	}
+
+	queue_lock_.unlock();
+}
+
+void
+ThreadQueue::Clear()
+{
+	QMutexLocker l(&queue_lock_);
+	while (!queued_threads_.isEmpty())
+		queued_threads_.dequeue();
+}
+}  // namespace threadpp
