934317c9a18ae2ae4c1a8017d48de3f5ba980872
Tonnerre LOMBARD <tonnerre@ancient-solutions.com>
2012-12-18 01:07:00 +0100

    Add a test case for the simple thread implementation.

---
+++ thread_test.cc
@@ -0,0 +1,58 @@
+/**
+ * Tests for the simple thread implementation.
+ */
+
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include "threadpool.h"
+
+namespace threadpp
+{
+namespace testing
+{
+
+class TestThread : public Thread
+{
+public:
+	explicit TestThread(QMutex* mtx, QWaitCondition* done)
+	: mtx_(mtx), done_(done), has_run_(false) {}
+
+	bool HasRun() {
+		return has_run_;
+	}
+
+protected:
+	virtual void Run() {
+		QMutexLocker l(mtx_);
+		Yield();
+		has_run_ = true;
+		done_->wakeAll();
+	}
+
+	QWaitCondition* done_;
+	QMutex* mtx_;
+	bool has_run_;
+};
+
+class ThreadTest : public ::testing::Test
+{
+};
+
+TEST_F(ThreadTest, DoesRun)
+{
+	QMutex mtx;
+	QWaitCondition done;
+
+	// Lock the done mutex to ensure we won't get the notification before
+	// we're running wait().
+	mtx.lock();
+
+	TestThread t(&mtx, &done);
+	Thread::Yield();
+	done.wait(&mtx);
+	EXPECT_TRUE(t.HasRun());
+}
+
+}  // namespace testing
+}  // namespace threadpp
